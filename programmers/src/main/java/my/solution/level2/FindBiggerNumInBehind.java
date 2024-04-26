214ced9ba31fc33e8d7529d6c010efd2bad3b2ec
sshyuny <thgustls@gmail.com>
2024-04-26 23:02:55 +0900

    feat: Lv2. 뒤에 있는 큰수 찾기 - 실패2

---
+++ programmers/src/main/java/my/solution/level2/FindBiggerNumInBehind.java
@@ -0,0 +1,90 @@
+package my.solution.level2;
+
+import java.util.*;
+
+/**
+ * 뒤에 있는 큰 수 찾기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/154539
+ */
+class FindBiggerNumInBehind {
+
+    /*
+     * 채점 결과: 실패(시간초과) - 테스트 22
+     */
+    class FindBiggerNumInBehindOutOfTime2 {
+        public int[] solution(int[] numbers) {
+            int length = numbers.length;
+            int[] answer = new int[length];
+            
+            Map<Integer, Integer> idxValueMap = new HashMap<>();
+            
+            for (int i = 0; i < length - 1; i++) {
+                findBiggerNum(numbers, answer, idxValueMap, i, length);
+            }
+            
+            if (idxValueMap.size() > 0) {
+                Set<Integer> ks = idxValueMap.keySet();
+                for (Integer curKey : ks) {
+                    answer[curKey] = -1;
+                }    
+            }
+                
+            answer[length - 1] = -1;
+            return answer;
+        }
+        
+        void findBiggerNum(int[] numbers, int[] answer, Map<Integer, Integer> idxValueMap, int idx, int length) {
+            int curNum = numbers[idx];
+            int nextNum = numbers[idx + 1];
+            
+            if (curNum >= nextNum) {
+                idxValueMap.put(idx, curNum);
+                return;
+            }
+            
+            answer[idx] = nextNum;
+            
+            if (idxValueMap.size() == 0) return;
+            
+            Iterator<Integer> iterator = idxValueMap.keySet().iterator();
+            while (iterator.hasNext()) {
+                Integer curKey = iterator.next();
+                if (numbers[curKey] >= nextNum) continue;
+                answer[curKey] = nextNum;
+                iterator.remove();
+            }
+        }
+    }
+
+    /*
+     * 채점 결과: 실패(시간초과) - 테스트 20 ~ 23
+     */
+    class FindBiggerNumInBehindOutOfTime1 {
+
+        int[] solution(int[] numbers) {
+            int length = numbers.length;
+            
+            int[] answer = new int[length];
+            
+            for (int i = 0; i < length; i++) {
+                int result = findBiggerNum(numbers, i, length);
+                answer[i] = result;
+            }
+            
+            return answer;
+        }
+        
+        int findBiggerNum(int[] numbers, int idx, int length) {
+            if (idx == length - 1) return -1;
+            int curNum = numbers[idx];
+            
+            for (int i = idx + 1; i < length; i++) {
+                int temp = numbers[i];
+                if (curNum < temp) return temp;
+            }
+            return -1;
+        }
+    }
+    
+
+}
--- programmers/src/test/java/my/solution/level2/FindBiggerNumInBehindTest.java
+++ programmers/src/test/java/my/solution/level2/FindBiggerNumInBehindTest.java
@@ -6,16 +6,31 @@ import java.util.Arrays;
 
 import org.junit.Test;
 
+import my.solution.level2.FindBiggerNumInBehind.FindBiggerNumInBehindOutOfTime1;
+import my.solution.level2.FindBiggerNumInBehind.FindBiggerNumInBehindOutOfTime2;
+
 public class FindBiggerNumInBehindTest {
 
-    FindBiggerNumInBehindOutOfTime findBiggerNumInBehindOutOfTime = new FindBiggerNumInBehindOutOfTime();
+    FindBiggerNumInBehind findBiggerNumInBehind = new FindBiggerNumInBehind();
+    FindBiggerNumInBehindOutOfTime1 findBiggerNumInBehindOutOfTime1 = findBiggerNumInBehind.new FindBiggerNumInBehindOutOfTime1();
+    FindBiggerNumInBehindOutOfTime2 findBiggerNumInBehindOutOfTime2 = findBiggerNumInBehind.new FindBiggerNumInBehindOutOfTime2();
     
     @Test
     public void test1() {
         int[] numbers = {2, 3, 3, 5};
         int[] rightAns = {3, 5, 5, -1};
         
-        int[] result = findBiggerNumInBehindOutOfTime.solution(numbers);
+        int[] result = findBiggerNumInBehindOutOfTime1.solution(numbers);
+
+        assertTrue(Arrays.equals(result, rightAns));
+    }
+
+    @Test
+    public void test2() {
+        int[] numbers = {2, 3, 3, 5};
+        int[] rightAns = {3, 5, 5, -1};
+        
+        int[] result = findBiggerNumInBehindOutOfTime2.solution(numbers);
 
         assertTrue(Arrays.equals(result, rightAns));
     }
